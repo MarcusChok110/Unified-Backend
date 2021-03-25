8b5463b46fbc302828bcab7ba38d6613fe1d38d2
Marcus Chok <marcus.chok110@gmail.com>
2021-03-25 10:39:21 -0400

    Initial commit

---
+++ src/main/java/com/marcuschok/UnifiedBackend/UnifiedBackendApplication.java
@@ -0,0 +1,13 @@
+package com.marcuschok.UnifiedBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnifiedBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnifiedBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcuschok/UnifiedBackend/UnifiedBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcuschok.UnifiedBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnifiedBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
