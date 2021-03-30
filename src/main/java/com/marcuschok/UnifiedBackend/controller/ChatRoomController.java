1cca3520d97487173f4ec6a70aa357e6ed81ef87
Marcus Chok <marcus.chok110@gmail.com>
2021-03-30 10:51:28 -0400

    Create initial structure for ChatRoom and Message controllers

---
+++ src/main/java/com/marcuschok/UnifiedBackend/controller/ChatRoomController.java
@@ -0,0 +1,9 @@
+package com.marcuschok.UnifiedBackend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/chatrooms")
+public class ChatRoomController {
+}
