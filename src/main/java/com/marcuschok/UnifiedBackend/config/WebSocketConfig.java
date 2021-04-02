c6b6e6a0481c6ce1c67d7355eae040f3e67d2771
Marcus Chok <marcus.chok110@gmail.com>
2021-04-01 21:06:32 -0400

    Configure allowed origins and remove sockjs from endpoint

--- src/main/java/com/marcuschok/UnifiedBackend/config/WebSocketConfig.java
+++ src/main/java/com/marcuschok/UnifiedBackend/config/WebSocketConfig.java
@@ -18,6 +18,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/unified-websocket").withSockJS();
+        registry.addEndpoint("/unified-websocket").setAllowedOrigins("*");
     }
 }
