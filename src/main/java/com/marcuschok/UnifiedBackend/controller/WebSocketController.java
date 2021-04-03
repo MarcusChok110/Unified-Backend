f95a26f0a046895622908b9bb2d0c5ba4f15a0c8
Marcus Chok <marcus.chok110@gmail.com>
2021-04-03 10:58:55 -0400

    Add endpoint for adding new messages

--- src/main/java/com/marcuschok/UnifiedBackend/controller/WebSocketController.java
+++ src/main/java/com/marcuschok/UnifiedBackend/controller/WebSocketController.java
@@ -1,6 +1,9 @@
 package com.marcuschok.UnifiedBackend.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.marcuschok.UnifiedBackend.model.Message;
+import com.marcuschok.UnifiedBackend.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -8,9 +11,19 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class WebSocketController {
 
+    private MessageService messageService;
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    public WebSocketController(MessageService messageService, ObjectMapper objectMapper) {
+        this.messageService = messageService;
+        this.objectMapper = objectMapper;
+    }
+
     @MessageMapping("/messages")
-    @SendTo("/topic")
-    public boolean socketResponse(Message message) {
-        return true;
+    @SendTo("/topic/responses")
+    public Message socketResponse(String messageJson) throws Exception {
+        Message message = this.objectMapper.readValue(messageJson, Message.class);
+        return this.messageService.save(message);
     }
 }
