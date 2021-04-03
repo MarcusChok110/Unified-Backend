00258a3c7b924772d12209093fb87aac9a9eb2ad
Marcus Chok <marcus.chok110@gmail.com>
2021-04-03 00:09:48 -0400

    Large refactor; delete user and chatroom components

--- src/main/java/com/marcuschok/UnifiedBackend/controller/ChatRoomController.java
+++
@@ -1,9 +0,0 @@
-package com.marcuschok.UnifiedBackend.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/chatrooms")
-public class ChatRoomController {
-}
