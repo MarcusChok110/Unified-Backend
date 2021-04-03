ca5083808fb81dd5b8e2870c256cf9cd27be26c4
Marcus Chok <marcus.chok110@gmail.com>
2021-04-03 10:18:04 -0400

    Add methods for finding all messages sorted by date and saving a new message

--- src/main/java/com/marcuschok/UnifiedBackend/service/MessageService.java
+++ src/main/java/com/marcuschok/UnifiedBackend/service/MessageService.java
@@ -1,8 +1,11 @@
 package com.marcuschok.UnifiedBackend.service;
 
+import com.marcuschok.UnifiedBackend.model.Message;
 import com.marcuschok.UnifiedBackend.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MessageService {
@@ -11,5 +14,13 @@ public class MessageService {
     @Autowired
     public MessageService(MessageRepository messageRepository) {
         this.messageRepository = messageRepository;
+    }
+
+    public List<Message> findAll() {
+        return this.messageRepository.findAllByOrderByTimestampAsc();
+    }
+
+    public Message save(Message message) {
+        return this.messageRepository.save(message);
     }
 }
