535324dc49de898bfeddcef3b1e75d533dd94dc6
Marcus Chok <marcus.chok110@gmail.com>
2021-04-03 10:18:30 -0400

    Add GET /messages endpoint for initial loading of chat

--- src/main/java/com/marcuschok/UnifiedBackend/controller/MessageController.java
+++ src/main/java/com/marcuschok/UnifiedBackend/controller/MessageController.java
@@ -1,9 +1,26 @@
 package com.marcuschok.UnifiedBackend.controller;
 
+import com.marcuschok.UnifiedBackend.model.Message;
+import com.marcuschok.UnifiedBackend.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/messages")
 public class MessageController {
+    private MessageService messageService;
+
+    @Autowired
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @GetMapping
+    public List<Message> getMessages() {
+        return this.messageService.findAll();
+    }
 }
