bb7f9564c278158cb4d8b7b9efcfe093b12ac428
Marcus Chok <marcus.chok110@gmail.com>
2021-04-05 11:35:47 -0400

    Convert tabs to spaces to keep consistent standard across files

--- src/main/java/com/marcuschok/UnifiedBackend/UnifiedBackendApplication.java
+++ src/main/java/com/marcuschok/UnifiedBackend/UnifiedBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UnifiedBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UnifiedBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UnifiedBackendApplication.class, args);
+    }
 
 }
