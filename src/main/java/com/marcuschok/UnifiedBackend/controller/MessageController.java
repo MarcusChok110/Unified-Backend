1cffe9a731be978bcc2cc4156dedef1631708338
Marcus Chok <marcus.chok110@gmail.com>
2021-04-05 11:35:19 -0400

    Add remaining REST endpoints for messages (GET /id, POST, PUT /id, DELETE /id)

--- src/main/java/com/marcuschok/UnifiedBackend/controller/MessageController.java
+++ src/main/java/com/marcuschok/UnifiedBackend/controller/MessageController.java
@@ -3,11 +3,10 @@ package com.marcuschok.UnifiedBackend.controller;
 import com.marcuschok.UnifiedBackend.model.Message;
 import com.marcuschok.UnifiedBackend.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/messages")
@@ -22,5 +21,42 @@ public class MessageController {
     @GetMapping
     public List<Message> getMessages() {
         return this.messageService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Message getMessage(@PathVariable Long id) {
+        Optional<Message> messageOptional = this.messageService.findById(id);
+
+        if (messageOptional.isEmpty()) {
+            throw new IllegalStateException("Message not found");
+        }
+
+        return messageOptional.get();
+    }
+
+    @PostMapping
+    public Message createMessage(@RequestBody Message message) {
+        return this.messageService.save(message);
+    }
+
+    @PutMapping("/{id}")
+    public Message editMessage(@RequestBody Message message, @PathVariable Long id) {
+        Optional<Message> messageOptional = this.messageService.findById(id);
+
+        if (messageOptional.isEmpty()) {
+            throw new IllegalStateException("Message not found");
+        }
+        message.setId(id);
+
+        return this.messageService.save(message);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMessage(@PathVariable Long id) {
+        Optional<Message> messageOptional = this.messageService.findById(id);
+        if (messageOptional.isEmpty()) {
+            throw new IllegalStateException("Message not found");
+        }
+        this.messageService.delete(messageOptional.get());
     }
 }
